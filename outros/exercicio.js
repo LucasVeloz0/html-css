367e732f9536eaf887bd06555991f429382b0fa0
LucasVeloz0 <122651147+LucasVeloz0@users.noreply.github.com>
2023-07-09 03:05:37 -0300

    continuando

---
+++ outros/exercicio.js
@@ -0,0 +1,22 @@
+let altitude = 18;
+
+
+    if (altitude >= 0 && altitude <= 20) {    
+        console.log("TROPOSFERA");
+        }
+        else if (altitude >= 21 && altitude <= 50) {
+            console.log("ESTRATOSFERA");
+        }
+        else if (altitude >= 51 && altitude <= 80) {
+            console.log("MESOSFERA");
+        }
+        else if (altitude >= 81 && altitude <= 450) {
+            console.log("TERMOSFERA");
+        }
+        else if (altitude >= 451 && altitude <= 900) {
+            console.log("EXOSFERA");
+        }
+   
+    
+    
+    
